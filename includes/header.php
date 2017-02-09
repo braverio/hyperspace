01d86a7822e51b9b55949e2381d39d5789ca3362
bnguyen654 <bnguyen9469@yahoo.com>
2017-02-08 23:12:27 -0800

    Pls no merge conflicts.

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,10 @@
 
 <head>
     <title>Hyperspace &raquo; <?php echo $title?></title>
+    <link rel="stylesheet" href="/styles/styles.css">
+    <link rel="stylesheet" href="/styles/mobile.css">
+    <link rel="stylesheet" href="/styles/responsive.css">
+    <link rel="icon" href="/images/favicon.jpg">
     <meta name="viewport" content="width=device-width, intial-scale = 1.0">
 </head>
 
@@ -11,30 +15,32 @@
     <nav id="navbar">
         <ul>
             <li>
-                <a href="/home"><img src="our logo lol"></a>
+                <a href="/home"><img src="/images/logo-med.png"></a>
             </li>
             <li>
                 <a href="/about">About</a>
+                <ul>
+                    <li> <a href="/about">About Us</a> </li>
+                    <li> <a href="/contact">Contact</a> </li>
+                    <li> <a href="/reviews">Reviews</a> </li>
+                </ul>
             </li>
-            <ul>
-                <li> <a href="/about">About Us</a> </li>
-                <li> <a href="/contact">Contact</a> </li>
-                <li> <a href="/reviews">Reviews</a> </li>
-            </ul>
             <li>
-                <a href="/attractions">What to Expect</a> </li>
-            <ul>
-                <li> <a href="/attractions">Games</a></li>
-                <li> <a href="/menu">Menu</a></li>
-                <li> <a href="/menu#snacks">Snack Bar</a></li>
-            </ul>
+                <a href="/attractions">What to Expect</a>
+                <ul>
+                    <li> <a href="/attractions">Games</a></li>
+                    <li> <a href="/menu">Menu</a></li>
+                    <li> <a href="/menu#snacks">Snack Bar</a></li>
+                </ul>
+            </li>
             <li>
-                <a href="/reserve">Reserve</a></li>
-            <ul>
-                <li><a href="/reserve">General Admission</a></li>
-                <li><a href="/packages">Party Packages</a></li>
-                <li><a href="/packages#birthday">Birthday Packages</a></li>
-            </ul>
+                <a href="/reserve">Reserve</a>
+                <ul>
+                    <li><a href="/reserve">General Admission</a></li>
+                    <li><a href="/packages">Party Packages</a></li>
+                    <li><a href="/packages#birthday">Birthday Packages</a></li>
+                </ul>
+            </li>
             <li>
                 <a href="/eclub">Join our E-Club</a>
             </li>
--- index.php
+++ index.php
@@ -6,6 +6,7 @@ include "$inc/header.php";
 ?>
 
 <div id="content-wrap">
+    Home Page
 </div>
 
 <?
