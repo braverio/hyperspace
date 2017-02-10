0f9be0acfdd061e8c510452176a97e4a8d878b61
bnguyen654 <bnguyen9469@yahoo.com>
2017-02-10 00:03:18 -0800

    Scaffolding for landing page

--- index.php
+++ index.php
@@ -28,17 +28,59 @@ include "$inc/header.php";
         <h1>What We Offer</h1>
         <div class="col-3 circle">
             <img src="https://placehold.it/100x100?text=what">
+            <h2>Family</h2>
+            <p>It's important to me that you're happy. Here's something that's fun. As trees get older they lose their chlorophyll. At home you have unlimited time.</p>
         </div>
         <div class="col-3 circle">
             <img src="https://placehold.it/100x100?text=what">
+            <h2>Parties</h2>
+            <p>There isn't a rule. You just practice and find out which way works best for you. There are no mistakes. You can fix anything that happens. It's beautiful - and we haven't even done anything to it yet. Let's do that again. And just raise cain.</p>
         </div>
         <div class="col-3 circle">
             <img src="https://placehold.it/100x100?text=what">
+            <h2>Food</h2>
+            <p>You have to allow the paint to break to make it beautiful. Only eight colors that you need. In your world you have total and absolute power. There's not a thing in the world wrong with washing your brush. It's cold, but it's beautiful. Let's make some happy little clouds in our world.</p>
         </div>
         <div class="col-3 circle">
             <img src="https://placehold.it/100x100?text=what">
+            <h2>Games</h2>
+            <p>Everything is happy if you choose to make it that way. Anyone can paint. The little tiny Tim easels will let you down. Everyone is going to see things differently - and that's the way it should be.</p>
         </div>
     </div>
+    
+    <div id="admissions" class="row">
+        <h1>General Admission</h1>
+        <div class="col-6">
+            <p>Just go ahead and load your Space Card with ateroids, where you can spend them on games. You may do this using our online system or just complete your transaction at the check-in desk. From these asteroids, you can gain tickets from playing and acheiving high scores!</p>
+        </div>
+        <div class="col-6">
+            <h3><span class="bold">100 Asteroids</span> $17.99</h3>
+            <h3><span class="bold">75 Asteroids</span> $14.99</h3>
+            <h3><span class="bold">50 Asteroids</span> $9.99</h3>
+            <h3><span class="bold">1 Asteroids</span> $0.25</h3>
+        </div>
+        <a href="/manage">Manage my Space Card</a>
+        
+    </div>
+    
+    <div class="row">
+        <h1>Party Packages</h1>
+    </div>
+    
+    <div class="row">
+        <h1>Games</h1>
+    </div>
+    
+    <div class="row">
+        <div class="col-6">
+            <h1>E-Club</h1>
+        </div>
+        <div class="col-6">
+            <h1>Reviews</h1>
+        </div>
+    </div>
+    
+    
 </div>
 
 <?
