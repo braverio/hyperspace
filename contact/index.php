b96d5e6d0ebd8ae6d73e0072bfb1a0806855df84
Joie <joie188@gmail.com>
2017-02-17 15:29:19 -0800

    dumb images

--- contact/index.php
+++ contact/index.php
@@ -23,9 +23,9 @@ if (empty($_SESSION['token'])) {
             <br>
             <p class="center-text">Monday to Thursday<br>11AM to 10PM<br><br>Friday to Sunday<br>9AM to 11PM</p>
             <br><br>
-            <img src="images/alien.png" style="height: 12em; margin-right: 1em;">
-            <img src="images/alien.png" style="height: 12em; margin-right: 1em;">
-            <img src="images/alien.png" style="height: 12em; margin-right: 1em;">
+            <img src="/images/alien.png" style="height: 12em; margin-right: 1em;">
+            <img src="/images/alien.png" style="height: 12em; margin-right: 1em;">
+            <img src="/images/alien.png" style="height: 12em; margin-right: 1em;">
         </div>
         <div class="col-6">
             <div class="form-container">
