71f4af085b28db1b2ced1b01c7b0dfcbd187d51e
Joie <joie188@gmail.com>
2017-02-18 18:24:04 -0800

    Fix text sizes on party

--- packages/index.php
+++ packages/index.php
@@ -9,14 +9,14 @@ include "$inc/header.php";
 <div class="navbaroffset"></div>
 <div clas="content-wrap container">
     <h1>Party Packages</h1>
-    <h2 class="center-text">Be sure to fill out the <a href="/reserve/">reservations form</a> to book a package! Special events like birthdays will come with an extra gift for the birthday star.</h2>
+    <h3 class="center-text">Be sure to fill out the <a href="/reserve/">reservations form</a> to book a package! Special events like birthdays will come with an extra gift for the birthday star.</h3>
     <div class="gaaa row">
         <div class="parallax parallax-38">
             <div class="black">
                 <div class="vertical-center">
                     <div style="margin-left: 1em; margin-right: 1em;">
                         <h1>Pluto</h1>
-                        <h2>Inlcudes one serving of a main and side per astronaut, unlimited drinks, and 30 asteroids</h2>
+                        <h3>Inlcudes one serving of a main and side per astronaut, unlimited drinks, and 30 asteroids</h3>
                         <h3>1 - 24 astronaut(s) $17.99 per<br> 25 - 50 astronauts $15.99 per<br> 51+ astronauts $13.99 per</h3>
                     </div>
                 </div>
@@ -27,7 +27,7 @@ include "$inc/header.php";
                 <div class="vertical-center">
                     <div style="margin-left: 1em; margin-right: 1em;">
                         <h1>Earth</h1>
-                        <h2>Inlcudes 2 servings of a main and one side per astronaut, unlimited drinks, goodie bag, and 50 asteroids</h2>
+                        <h3>Inlcudes 2 servings of a main and one side per astronaut, unlimited drinks, goodie bag, and 50 asteroids</h3>
                         <h3>1 - 24 astronaut(s) $21.99 per<br> 25 - 50 astronauts $19.99 per<br> 51+ astronauts $17.99 per</h3>
                     </div>
                 </div>
@@ -38,7 +38,7 @@ include "$inc/header.php";
                 <div class="vertical-center">
                     <div style="margin-left: 1em; margin-right: 1em;">
                         <h1>Jupiter</h1>
-                        <h2>All-you-can-eat mains and sides, unlimited drinks, goodie bag, and 100 asteroids</h2>
+                        <h3>All-you-can-eat mains and sides, unlimited drinks, goodie bag, and 100 asteroids</h3>
                         <h3>1 - 24 astronaut(s) $25.99 per<br> 25 - 50  astronauts $23.99 per<br> 51+ astronauts $21.99 per</h3>
                     </div>
                 </div>
